5826680275807a68a8c25eba219ab752166150ab
unknown <tanmayrsm@gmail.com>
2020-06-28 20:12:08 +0530

    first

---
+++ src/config/rest_config.js
@@ -0,0 +1,5 @@
+export const  articles_url = "https://newsapi.org/v2/top-headlines/";
+export const language = "en";
+export const category = "general";
+
+export const _api_key = "09c6e6ba28b0407980b18a72bd2d0f5e";
